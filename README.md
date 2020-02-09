a2c93c8abcda0d4dc7f405f4d90d285a417e433e
rafasilvasousa <rafaelsousa.ja.adv@gmail.com>
2020-02-09 18:22:59 -0300

    Implementação 01

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# graphs
+# Teoria dos Grafos
 Atividades de Implementação da Disciplina de Teoria dos Grafos
+-
+Lista de arquivos:
+    
+    + graph.py - Arquivo da Atividade de Implementação 01
+        O código está implementado ja com um conjunto de nós e arestas. Para testar o app será necessário remover a marcação de comentário no bloco de código das linhas 15-21, bem como comentar o bloco de código das linhas 27-30
+        O padrão de entradas deve seguir os exemplos a seguir:
+        
+        Vértices: {a, b, c, d, e, f}
+        entrada: a,b,c,d,e,f (sem espaços)
+
+        arestas {(a,b), (c,a), (b,c)}
+        entrada: ab,ca,bc
+
+        Após a inserção dos vertices, será impresso a matriz de adjacencia e em seguida a lista de adjacencias de cada nó. O código finaliza com a exibição de um gráfico simples.
+
+        **Importante**: Esse código foi testado usando caracteres como entradas para vertices e conjuntos de arestas. Não há evidências de que funcione com outro tipo de dados.
+    + 
--- graph.py
+++ graph.py
@@ -7,18 +7,16 @@ def gera_lista(G, n):
     m=[]
     m.extend(G.neighbors(n))
     return m
-'''
-def gera_matriz(G, n):
-''' 
+
+def gera_matriz(G):
+    m=nx.adjacency_matrix(G)
+    return(m.toarray())
     
 ###################PARA TESTES HABILITAR ESSE TREECHO DE CODIGO #################################################################
-# print("Escreva a lista dos nós separados por vírgula: ")
-# nodes = input().split(",")
-# print(type(nodes))
-# print("Escreva a lista das arestas no formato ab separados por vírgula: ")
-# edges = input().split(",")
-# print(type(edges))
-# print(edges)
+print("Escreva a lista dos nós separados por vírgula: ")
+nodes = input().split(",")
+print("Escreva a lista das arestas no formato ab separados por vírgula: ")
+edges = input().split(",")
 ##############################################################################################
 
 # lista de nós para testes: [a, b, c, d, e, f] -> entrada a,b,c,d,e,f
@@ -26,19 +24,19 @@ def gera_matriz(G, n):
 # entrada ab,de,ce,bc,ef
 
 ########################PARA TESTES DESABILITAR ESSE TRECHO DE CODIGO##################
-nodes='a', 'b', 'c', 'd', 'e', 'f'
-edges='ab','de','ce','bc','ef'
+# nodes='a', 'b', 'c', 'd', 'e', 'f'
+# edges='ab','de','ce','bc','ef'
 #####################################################################################
 
 G = nx.Graph()
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
 
-# for i in G.nodes:
-#     print("Lista de adjacencias de "+i+": "+ str(gera_lista(G,i)))
+print("Matriz de adjacencia:")
+print(gera_matriz(G))
 
-m=nx.adjacency_matrix(G)
-print(m.toarray())
+for i in G.nodes:
+    print("Lista de adjacencias de "+i+": "+ str(gera_lista(G,i)))
 
 
 nx.draw_networkx(G,with_labels=True, pos=nx.circular_layout(G), node_color='r', edge_color='b')
