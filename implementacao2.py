152d7d490d408088110f02543dd15159a07bb67a
Rafael Silva de Sousa <rafasilvasousa@MacBook-Air-de-Diego.local>
2020-02-16 09:01:26 -0300

    AJUSTES DE SAIDA

--- implementacao2.py
+++ implementacao2.py
@@ -1,27 +1,15 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-################# FUNÇÃO PARA IMPRIMIR BUSCA PROF####################
-def imprimir_profundidade(G:nx.Graph):
-    print('Busca por profundidade')
-    temp = busca_profundidade(G)
+################# FUNÇÃO PARA IMPRIMIR ####################
+def imprimir(temp):
     caminho=temp[0]
     componentes = temp[-1]
-    print('Componentes Conexos: '+str(componentes))
-    if(componentes==1):
-        print('Caminho: ', end='')
-        for i in caminho[0]:
-            print(str(i)+" ", end='')
-        print()
-
-    else:
-        for i in range(componentes):
-            print('Caminho do comp '+str(i+1)+': ', end='')
-            for j in caminho[i]:
-                print(str(j)+" ", end='')
-            print()
-
-#####################################################################
+    for i in range(componentes):
+        print('Componente: '+str(i+1))
+        for j in caminho[i]:
+            print(str(j)+" ", end='')
+        print('\n---------------------')
 
 ###################### FUNÇÃO DE BUSCA POR PROFUNDIDADE #############
 def busca_profundidade(G: nx.Graph):
@@ -30,13 +18,10 @@ def busca_profundidade(G: nx.Graph):
     vetor_aux=[]
     comp_conexas = 0
     def prof(v):
-        # print(v)
-        #visitados.append(v)
         for i in G.neighbors(v):
             if i not in visitados:
                 visitados.append(i)
                 vetor_aux.append(i)
-                #saida.append(i)
                 prof(i)
 
     for i in G.nodes:
@@ -47,33 +32,12 @@ def busca_profundidade(G: nx.Graph):
             comp_conexas+=1
             vetor_aux.append(i)
             visitados.append(i)
-            # saida.append(i)
             prof(i)
     saida.append(vetor_aux.copy())
     return (saida, comp_conexas)
 
 #####################################################################
 
-################# FUNÇÃO PARA IMPRIMIR BUSCA AMPL####################
-def imprimir_amplitude(G:nx.Graph):
-    print('Busca por amplitude')
-    temp = busca_largura(G)
-    caminho=temp[0]
-    componentes = temp[-1]
-    print('Componentes Conexos: '+str(componentes))
-    if(componentes==1):
-        print('Caminho: ', end='')
-        for i in caminho:
-            print(str(i)+" ", end='')
-        print()
-    else:
-        for i in range(componentes):
-            print('Caminho do comp '+str(i+1)+': ', end='')
-            for j in caminho[i]:
-                print(str(j)+" ", end='')
-            print()
-
-#####################################################################
 
 ###################### FUNÇÃO DE BUSCA POR AMPLITUDE ################
 def busca_largura(G:nx.Graph):
@@ -114,7 +78,7 @@ def busca_largura(G:nx.Graph):
 
 
 ######################## FUNÇÃO PRINCIPAL ###########################
-arquivo = open('grafo1', 'r')
+arquivo = open('grafo2', 'r')
 nvertices =int(arquivo.readline())
 
 vertices = tuple(arquivo.readline().split())
@@ -122,6 +86,7 @@ vertices = tuple(arquivo.readline().split())
 narestas = int(arquivo.readline())
 
 lista_arestas = []
+
 for i in range(narestas):
     lista_arestas.append(arquivo.readline().strip().replace(' ', ''))
 
@@ -131,10 +96,16 @@ grafo = nx.Graph()
 grafo.add_nodes_from(vertices)
 grafo.add_edges_from(lista_arestas)
 
-imprimir_profundidade(grafo)
+profundidade=busca_profundidade(grafo)
+print("--> Algoritmo DFS")
+imprimir(profundidade)
 print()
 print()
-imprimir_amplitude(grafo)
+print('=================================================')
+print()
+print("--> Algoritmo BFS")
+largura=(busca_largura(grafo))
+imprimir(largura)
 
 
 nx.draw_networkx(grafo,with_labels=True, 
